b04fd88d19ae5e0828b5e46a97bdc2f4c9a27e16
Victor Borja <vborja@apache.org>
2025-10-20 17:00:25 -0600

    dendritic template enables vic/den

--- modules/dendritic/dendritic.nix
+++ modules/dendritic/dendritic.nix
@@ -5,16 +5,17 @@
     (inputs.flake-parts.flakeModules.modules or { })
     (inputs.flake-file.flakeModules.import-tree or { })
     (inputs.flake-aspects.flakeModule or { })
+    (inputs.den.flakeModule or { })
   ];
 
   flake-file.inputs = {
     flake-parts.url = lib.mkDefault "github:hercules-ci/flake-parts";
     flake-aspects.url = lib.mkDefault "github:vic/flake-aspects";
+    den.url = lib.mkDefault "github:vic/den";
   };
 
   flake-file.outputs = ''
-    inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; }
-    (inputs.import-tree ./modules)
+    inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)
   '';
 
 }
