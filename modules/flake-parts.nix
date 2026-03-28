953d01f3ae5ba50869c5e1248062198f73e971bf
David Chocholatý <chocholaty.david@protonmail.com>
2026-03-28 03:49:35 +0100

    feat: Expose `flake-file.apps` as flake outputs `apps` instead of `packages` (#92)

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -3,7 +3,7 @@
   perSystem =
     { pkgs, ... }:
     {
-      packages = lib.mapAttrs (_: f: f pkgs) config.flake-file.apps;
+      apps = lib.mapAttrs (_: f: { program = f pkgs; }) config.flake-file.apps;
       checks.check-flake-file = config.flake-file.check-flake-file pkgs;
     };
 }
