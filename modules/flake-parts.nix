2dc06a36f09c9a765dcec4d22d8d514b42646dcf
Michael Utz <michael@theutz.com>
2026-04-13 16:09:47 +0300

    feat: output write-* to packages instead of apps

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -3,7 +3,7 @@
   perSystem =
     { pkgs, ... }:
     {
-      apps = lib.mapAttrs (_: f: { program = f pkgs; }) config.flake-file.apps;
+      packages = lib.mapAttrs (_: f: f pkgs) config.flake-file.apps;
       checks.check-flake-file = config.flake-file.check-flake-file pkgs;
     };
 }
